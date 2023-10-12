273e45412556b15bb5bb6bc33179880667048e5a
Eliver Salazar Campos <140202853+Eliver-Salazar@users.noreply.github.com>
2023-10-12 15:48:35 -0600

    Sem 05
    Se agrega la semana 05 completa

--- NetBeansProjects/IntroPrograClase1/src/main/java/Semana5/Estudiante.java
+++ NetBeansProjects/IntroPrograClase1/src/main/java/Semana5/Estudiante.java
@@ -2,11 +2,36 @@ package Semana5;
 
 import javax.swing.JOptionPane;
 
-public class Estudiante{
+public class Estudiante {
+    public String nombreCompleto;
+    public int edad;
+    public int creditosCursados = 0;
+    public Carrera carrera;
     
-    //Class Baño
+    public Estudiante(String nombreCompleto, int edad, int creditosCursados, Carrera carrera){
+        this.nombreCompleto = nombreCompleto;
+        this.edad = edad;
+        this.creditosCursados = creditosCursados;
+        this.carrera = carrera;
+        this.ImprimeEnConsola("NombreCompleto: "+nombreCompleto+", Edad: "+edad+", Carrera: "+ this.carrera.nombre+", Creditos: "+this.carrera.creditos);
+    }
     
+    public int CalcularCreditosRestantes(){
+        //calcula creditos restantes
+        return  this.carrera.creditos - this.creditosCursados;
+    }
+    
+    public void ImprimeEnConsola(String texto){
+        System.out.println(texto);
+    }
+    
+    
+    public int CalculaCuatrimestresRestantes(){
+        //Cada cuatrimestre cursa 16 creditos
+        int creditosRestantes = this.CalcularCreditosRestantes();
         
+        //redondeo para casos donde creditos sean menor or mayor que un multiplo de 16
+        return Math.round((float) creditosRestantes/16);
     }
 }
 
---
+++ NetBeansProjects/IntroPrograClase1/src/main/java/Semana5/Sem05.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Semana5;
+
+/**
+ *
+ * @author Eliver Salazar Campo
+ */
+public class Sem05 {
+    public static void main(String[] args){
+        Estudiante estudiante = new Estudiante("Pedro Gonzalez", 25, 13, new Carrera(58,"Ing. Sistemas"));
+        
+        estudiante.ImprimeEnConsola("Creditos restantes: "+ estudiante.CalcularCreditosRestantes());
+        estudiante.ImprimeEnConsola("Estimacion de cuatrimestres restantes: "+estudiante.CalculaCuatrimestresRestantes());
+    }
+    
+}
